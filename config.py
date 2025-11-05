e6913f5179bd46bfe8945c50e93a1564da27a827
palaciossusang-debug <palaciossusang@gmail.com>
2025-11-05 12:44:32 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from langchain_openai import ChatOpenAI
 
 
 # Carga la clave desde Streamlit Secrets o variable de entorno
-#OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY", os.getenv("OPENAI_API_KEY"))
+OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY", os.getenv("OPENAI_API_KEY"))
 
 # Configuración del modelo LLM
 llm = ChatOpenAI(
     model="gpt-4o",
-    openai_api_key=getpass.getpass("Ingresa tu API Key de OpenAI : ")
+    openai_api_key=OPENAI_API_KEY 
 )
